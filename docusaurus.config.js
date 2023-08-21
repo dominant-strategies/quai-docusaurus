0b4b9e615b16fc92a18cc5a187a78792b547af6c
Connor Hubbard <connor@dominantstrategies.io>
2023-08-21 15:46:00 -0500

    added Dockerfile; helm chart; reset version

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
 	// organizationName: 'dominant-strategies', // Usually your GitHub org/user name.
 	// projectName: 'quai-docs', // Usually your repo name.
 
-	onBrokenLinks: 'throw',
+	onBrokenLinks: 'ignore',
 	onBrokenMarkdownLinks: 'warn',
 
 	// Even if you don't use internalization, you can use this field to set useful
